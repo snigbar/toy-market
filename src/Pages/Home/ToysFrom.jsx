66ea13618ae93f00d414840fe1e1d646c3622e07
snigbar <snigbar@outlook.com>
2023-05-20 02:29:31 +0600

    react slik used for section toys from

---
+++ src/Pages/Home/ToysFrom.jsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import Slider from 'react-slick';
+import img1 from '../../assets/toys-from/amazon.png'
+import img2 from '../../assets/toys-from/dc.png'
+import img3 from '../../assets/toys-from/greenlantern.png'
+import img4 from '../../assets/toys-from/incredibles.png'
+import img5 from '../../assets/toys-from/marvel.png'
+import img6 from '../../assets/toys-from/ninja.png'
+import img7 from '../../assets/toys-from/venom.png'
+import img8 from '../../assets/toys-from/xmen.png'
+
+
+
+const ToysFrom = () => {
+
+    var settings = {
+
+      infinite: true,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplay: true,
+      speed: 3000,
+      autoplaySpeed: 5,
+      cssEase: "linear",
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+    
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+            
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+      };
+
+  return (
+    <section className='py-8 my-8'>
+    <div className='w-3/5 mx-auto'>
+    <h1 className='text-5xl text-center text-zinc-800 font-bold leading-[4rem]'>Unleash The Hero Within!</h1>
+    <p className='text-center my-8 text-2xl tracking-[3px]'>Discover the Origins of Our Action Figure Toys</p>
+    </div>
+
+    <Slider {...settings} className='w-11/12 lg:w-4/5 mx-auto my-4'>
+    <div className='text-center'>
+      <img src={img1} className='w-44 h-40'/>
+    </div>
+    <div className='text-center'>
+      <img src={img2} className='w-44 h-40'/>
+    </div>
+    <div className='text-center'>
+      <img src={img3} className='w-44 h-40'/>
+    </div>
+    <div className='text-center'>
+      <img src={img4} className='w-44 h-40'/>
+    </div>
+    <div className='text-center'>
+      <img src={img5} className='w-44 h-40'/>
+    </div>
+    <div className='text-center'>
+      <img src={img6} className='w-28 lg:w-44 h-40'/>
+    </div>
+    <div className='text-center'>
+      <img src={img7} className='w-44 h-40'/>
+    </div>
+    <div className='text-center'>
+      <img src={img8} className='w-44 h-40'/>
+    </div>
+  </Slider>
+  </section>
+  )
+}
+
+export default ToysFrom
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
 import { RouterProvider } from 'react-router-dom'
 import router from './Routes/Router'
 
