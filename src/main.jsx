ea95583aa33667992e83363e2e669e8e96357d5f
snigbar <snigbar@outlook.com>
2023-05-19 18:00:19 +0600

    flowbite removed and daisyUI installed due unwanted issues

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,10 @@ import router from './Routes/Router'
 
 
   ReactDOM.createRoot(document.getElementById("root")).render(
+    <div className='max-w-screen-2xl mx-auto mt-2'>
     <React.StrictMode>
       <RouterProvider router={router} />
     </React.StrictMode>
+    </div>
   )
 
