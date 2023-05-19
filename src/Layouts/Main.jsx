55a2e9b41a9e113686f42587c9408821b43ae80f
snigbar <snigbar@outlook.com>
2023-05-19 20:38:51 +0600

    header footer added

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../Components/Navbar/Navbar'
 import { Outlet } from 'react-router-dom'
+import Footer from '../Components/Footer/Footer'
 
 const Main = () => {
   return (
     <>
     <Navbar></Navbar>
     <Outlet></Outlet>
+    <Footer></Footer>
     </>
   )
 }
